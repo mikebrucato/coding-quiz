e2ef08eebc5541d5a4a578ddf9239ab07800dd50
mikebrucato <brucatomike1@gmail.com>
2020-10-19 17:09:56 -0500

    added questions and answers to an array object

--- script.js
+++ script.js
@@ -17,4 +17,46 @@ var questions = document.querySelector(".questions")
 var answers = document.querySelector(".answersList")
 var startButton = document.querySelector(".startButton")
 
-var
+var quizQuestions = [
+{
+    question: "What are variables in Javascript used for?",
+    answers: {
+        a: "to store data",
+        b: "math functions",
+        c: "to loop",
+    },
+    correctAnswer: "a"
+},
+{   question: "What function is used to return an integer?",
+    answers: {
+        a: "math.random()",
+        b: "math.floor()",
+        c: "upperCase()",
+    },
+    correctAnswer: "b"
+},
+{   question: "What are prompts used for?",
+    answers: {
+        a: "to get an input from a user",
+        b: "to alert the user",
+        c: "to provide a messege in the console",
+    },
+    correctAnswer: "a"
+},
+{   question: "What are booleans in Javascript?",
+    answers: {
+        a: "any number that's a non-integer",
+        b: "a type of math function",
+        c: "a true or false statement",
+    },
+    correctAnswer: "c"
+},
+{   question: "which method returns the length of a string?",
+    answers: {
+        a: "size()",
+        b: "length()",
+        c: "index()",
+    },
+    correctAnswer: "b"
+}
+]
