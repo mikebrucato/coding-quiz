e41705797a03a12059b8e5901cc1f7e4bf583e7e
mikebrucato <brucatomike1@gmail.com>
2020-10-19 15:43:45 -0500

    create main html variables

---
+++ script.js
@@ -0,0 +1,20 @@
+// create variables for html classes
+
+// create object array for questions and answers
+
+// create timer for countdown button, the the button is clicked the timer counts down
+
+// create scoring system
+
+// create a way for scores to be stored
+
+// when a questions is answered, the page refreshed to the next question
+
+
+
+var timer = document.querySelector(".timer")
+var questions = document.querySelector(".questions")
+var answers = document.querySelector(".answersList")
+var startButton = document.querySelector(".startButton")
+
+var
